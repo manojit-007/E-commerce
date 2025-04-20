3cbfd13faa76eb3ed2db8c6bf7563da0511d301c
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2025-04-20 14:31:24 +0530

    fix cors

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
